55dd6a263ff34dfdfa634cb4e8860c8ba1e50630
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-02-21 19:21:05 +0530

    new file created

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,1 +1,13 @@
-#/
+#!/bin/bash
+
+### Special variables ###
+echo "All args passed to script: $@"
+echo "Number of vars passed to script: $#"
+echo "Script name: $0"
+echo "Who is running: $PWD"
+echo "Home directory of current user: #USER"
+echo "Home directory of current user: $HOME"
+echo "PID of this script: $$"
+sleep 100 &
+echo "PID of recently executed background process: $!"
+echo "All args passed to script: $*"
